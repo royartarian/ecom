3e2cbc014900b042a3324099eefdf7188a193232
royartarian <royartarian@gmail.com>
2020-02-10 17:26:05 -0500

    v1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const usersRepo = require("./repositories/users");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <div> 
+        <form method="POST">
+            <input name="email" placeholder="email" /> 
+            <input name="password" placeholder="password" /> 
+            <input name="passwordConfirmation" placeholder="password confirmation" /> 
+            <button>Sign Up </button>
+        </form>
+    </div>
+  `);
+});
+
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+  if(password !== passwordConfirmation){
+    return res.send("Passwords must match")
+  }
+  res.send(`
+        Account Created.
+    
+    `);
+});
+app.listen(3000, () => {
+  console.log("Listening");
+});
