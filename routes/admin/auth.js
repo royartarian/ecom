d8fff01d46a2b581f13fc8516d0cc679afb5625f
royartarian <royartarian@gmail.com>
2020-03-09 23:08:39 -0400

    validation module

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
+const {requireEmail, requirePassword,requirePasswordConfirmation} = require('./validators');
+
 
 const router = express.Router();
 
@@ -12,17 +14,16 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  [check("email").isEmail(), check("password"), check("passwordConfirmation")],
+  [
+    requireEmail,
+    requirePassword,
+    requirePasswordConfirmation
+  ],
   async (req, res) => {
-    const { email, password, passwordConfirmation } = req.body;
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-      return res.send("Email in use");
-    }
-    if (password !== passwordConfirmation) {
-      return res.send("Passwords must match");
-    }
+    const errors = validationResult(req);
+    console.log(errors);
 
+    const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
